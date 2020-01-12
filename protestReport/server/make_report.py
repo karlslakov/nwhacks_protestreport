50a0a5230b9ecdf6b289d3a4d0bf6227ec760018
Kiara <kiarag@alumni.ubc.ca>
2020-01-11 16:47:52 -0800

    can get data from twitter

--- protestReport/server/make_report.py
+++ protestReport/server/make_report.py
@@ -9,7 +9,8 @@ from secrets import SECRETS
 def make_report(keywords, date):
     client = AppClient(SECRETS['CONSUMER_KEY'],
                     SECRETS['CONSUMER_SECRET'])
-    client.get_access_token
-    or_together_keywords = urllib.parse.quote(keywords.replace(',','OR'))
-    response = client.make_api_call('GET','https://api.twitter.com/1.1/search/tweets.json',q=or_together_keywords)
-    
+    client.get_access_token()
+    or_together_keywords = urllib.parse.quote(keywords.replace(',',' OR '))
+    # response = client.make_api_call('GET','https://api.twitter.com/1.1/search/tweets.json',data=or_together_keywords)
+    response = client.api.search.tweets.get(q=or_together_keywords)
+    print(json.dumps(response.data))
